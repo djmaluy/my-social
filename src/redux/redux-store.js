9bc6039a2b3ecd0ea5e18abc7eb0bd0fc8fcdffd
Andrii Tsvirko <a.tsvirko@ukr.net>
2020-06-07 07:15:03 +0200

    Added login form

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -5,6 +5,7 @@ import sidebarReducer from "./sidebar-reducer";
 import usersReducer from "./users-reducer";
 import authReducer from "./auth-reducer";
 import thunkMiddleware from "redux-thunk";
+import { reducer as formReducer } from "redux-form";
 
 let reducers = combineReducers({
   // это функция, у которой внутри обьект
@@ -13,6 +14,7 @@ let reducers = combineReducers({
   sidebar: sidebarReducer, // здесь мы комбайним три наших reducers from our Store
   usersPage: usersReducer,
   auth: authReducer,
+  form: formReducer,
 });
 
 let store = createStore(reducers, applyMiddleware(thunkMiddleware));
